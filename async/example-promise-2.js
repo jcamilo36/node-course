147329c305f480134a0bb29d5ebd6450c2171ca8
Juan Cortes <jcortes@hugeinc.com>
2016-09-27 16:58:08 -0500

    End advanced promises tutorial.

--- async/example-promise-2.js
+++ async/example-promise-2.js
@@ -1,17 +1,33 @@
 function doWork(shouldFail) {
 	return new Promise(function (resolve, reject) {
 		setTimeout(function() {
-			if (typeof shouldFail === 'boolean' && shouldFail === true) :
-			console.log('done!');
-			resolve();
+			if (typeof shouldFail === 'boolean' && shouldFail === true) {
+				reject('error message');
+			} else {
+				resolve('success');
+			} 
 		}, 1000);
 	});
 }
 
-doWork().then(function () {
+// with then and 2 params
+//doWork(true).then(function (message) {
+//	console.log(message);
+//	return doWork(true);
+//}).then(function (message) {
+//	console.log(message);
+//	console.log('all done!');
+//}, function (error) {
+//	console.log(error);
+//});
+	
+
+doWork(true).then(function (message) {
+	console.log(message);
 	return doWork(true);
-}).then(function () {
+}).then(function (message) {
+	console.log(message);
 	console.log('all done!');
-}, function (error) {
+}).catch(function (error) {
 	console.log(error);
 });
