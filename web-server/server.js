85a8f407b946c7dfd63fba0e961844f223270bdc
juancamilo <jcamilo.36@gmail.com>
2016-10-15 20:19:44 -0500

    middleware

--- web-server/server.js
+++ web-server/server.js
@@ -2,12 +2,26 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-app.get('/about', function(request, response) {
-  response.send('About us');
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+    console.log('private route hit');
+    next();
+	},
+  logger: function(req, res, next) {
+    console.log('Request: [' + new Date().toString() + '] ' + req.method + ' ' + req.originalUrl);
+    next();
+  } 
+};
+
+app.use(middleware.logger);
+// Require authentication for all pages.
+//app.use(middleware.requireAuthentication);
+app.get('/about', middleware.requireAuthentication, function(request, response) {
+	response.send('About us');
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
-  console.log('Express server started on port ' + PORT);
+	console.log('Express server started on port ' + PORT);
 });
