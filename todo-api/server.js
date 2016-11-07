3df8ce351ca3920bd1ea8def19e001fd16047f08
juancamilo <jcamilo.36@gmail.com>
2016-11-07 18:24:35 -0500

    Add post /todos

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Meet mom for lunch',
-  complete: false
-}, {
-  id: 2,
-  description: 'Go to market',
-  complete: false
-}, {
-  id: 3,
-  description: 'Feed the dog',
-  complete: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -40,6 +32,14 @@ app.get('/todos/:id', function (req, res) {
   } else {
     res.sendStatus(404);
   }
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+  var body = req.body;
+  body.id = todoNextId++;
+  todos.push(body);
+  res.json(body);
 });
 
 app.listen(PORT, function() {
