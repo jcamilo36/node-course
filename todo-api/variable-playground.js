2695d25dace9da4641cced662a3907cd1b10a168
Juan Cortes <jcortes@hugeinc.com>
2016-11-15 16:38:02 -0500

    filter by completed

---
+++ todo-api/variable-playground.js
@@ -0,0 +1,22 @@
+//var person = {
+//  name: 'Andrew',
+//  age: 21
+//};
+//
+//function updatePerson(obj) {
+//  obj.age = 24;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array example
+var grades = [25,27];
+
+function addGrades (grades) {
+  grades.push(26);
+  debugger;
+}
+
+addGrades(grades);
+console.log(grades);
