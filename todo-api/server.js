9cb6aef02128cc3cd0cf50cf1a5f64e1f9477d9e
Juan Cortes <jcortes@hugeinc.com>
2017-02-15 10:35:28 -0500

    Delete todo with sequelize

--- todo-api/server.js
+++ todo-api/server.js
@@ -106,13 +106,27 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  if (matchedTodo) {
-    todos = _.without(todos, matchedTodo);
-    res.json(todos);
-  } else {
-    res.status(404).json({"error": "no todo found with that id"});
-  }
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then(function(rowsDeleted) {
+    if (rowsDeleted > 0) {
+      res.status(204).send();
+    } else {
+      res.status(400).json({"error": "no todo found with that id"});
+    }
+  }, function() {
+    res.status(500).send();
+  });
+
+  //var matchedTodo = _.findWhere(todos, {id: todoId});
+  //if (matchedTodo) {
+  //  todos = _.without(todos, matchedTodo);
+  //  res.json(todos);
+  //} else {
+  //  res.status(404).json({"error": "no todo found with that id"});
+  //}
 });
 
 // PUT /todos/:id
