b560dc926ba319e0d1457cc615c39561766b7e01
Juan Cortes <jcortes@hugeinc.com>
2017-03-15 15:33:04 -0500

    Playground: associations

--- todo-api/playground/basic-sqlite-database.js
+++ todo-api/playground/basic-sqlite-database.js
@@ -19,19 +19,42 @@ var Todo = sequelize.define('todo', {
   }
 });
 
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync(
   // To wipe the database.
   //{force: true}
 ).then(function() {
   console.log('Everything is synced');
 
-  Todo.findById(3).then(function (todo) {
-    if (todo) {
-      console.log(todo.toJSON());
-    } else {
-      console.log('Not found');
-    }
+  User.findById(1).then(function (user) {
+    user.getTodos({
+      where: {
+        completed: false
+      }
+    }).then(function (todos) {
+      todos.forEach(function (todo) {
+        console.log(todo.toJSON());
+      });
+    });
   });
+
+  //User.create({
+  //  email: 'jcortes@hugeinc.com'
+  //}).then(function() {
+  //  return Todo.create({
+  //    description: 'Clean yard'
+  //  }).then(function (todo) {
+  //    User.findById(1).then(function (user) {
+  //      user.addTodo(todo);
+  //    });
+  //  })
+  //});
 
   //Todo.create({
   //  description: "Take out trash",
